417295e52ecb6bee10be41ec63dde1e93f47d66a
Jacob <jacobrashid99@gmail.com>
2024-04-24 16:39:48 -0700

    implemented show_completed_story

--- app.py
+++ app.py
@@ -20,3 +20,24 @@ def show_story_form():
         "questions.jinja",
         prompts=required_questions
     )
+
+
+@app.get("/results")
+def show_completed_story():
+    """gets prompt answers from query string, 
+    creates dictionary with promt:answer pairs,
+    generates story using answers dictionary"""
+
+    answers = {}
+    for promt in silly_story.prompts:
+        input = request.args[promt]
+        answers[promt] = input
+
+    print("answers", answers)
+
+    story_text = silly_story.get_result_text(answers)
+
+    return render_template(
+        "results.jinja",
+        story=story_text
+    )
