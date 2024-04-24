f2114945cc4782a0ce109c4ce45c0eefe8844300
Andrea Jao <ndrjao@gmail.com>
2024-04-24 16:04:40 -0700

    updated questions.jinja with form render & app.py with page load get

--- app.py
+++ app.py
@@ -7,3 +7,16 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+
+@app.get("/")
+def show_story_form():
+    """at the load of the home page, get the fields needed to initialize
+        as silly story instance, to show a respective prompts form"""
+
+    required_questions = silly_story.prompts
+
+    return render_template(
+        "questions.jinja",
+        prompts=required_questions
+    )
